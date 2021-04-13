3db9856c8fb0e7d3adba5c6c8d2b1165d1416911
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2021-04-12 23:03:16 -0700

    undoing with random comment

--- part2.js
+++ part2.js
@@ -5,7 +5,8 @@ const table = document.querySelector("div");
 const baseURL = "https://deckofcardsapi.com/api/deck/";
 const shuffleURL = baseURL + "new/shuffle/?deck_count=1";
 let deckID = null;
-let cardCount = 0;
+let cardsArray;
+let cards;
 
 draw.style.marginBottom = "4rem";
 shuffle.style.marginBottom = "4rem";
@@ -14,40 +15,48 @@ const degrees = () => {
 	return Math.floor(Math.random() * 360 + 1);
 };
 
+const rotate = () => {
+	return `rotate(${degrees()}deg)`;
+};
+
 draw.style.display = "none";
 
 shuffle.addEventListener("click", () => {
 	axios.get(shuffleURL)
-		.then((res) => (deckID = res.data.deck_id))
+		.then((res) => {
+			deckID = res.data.deck_id;
+			const drawURL = baseURL + deckID + "/draw/?count=52";
+			return axios.get(drawURL);
+		})
+		.then((res) => {
+			cardsArray = res.data.cards;
+			cards = cardsArray[Symbol.iterator]();
+		})
 		.catch((err) => console.log(err));
 	draw.style.display = "block";
 	shuffle.style.display = "none";
 });
 
 draw.addEventListener("click", () => {
-	const drawURL = baseURL + deckID + "/draw/?count=1";
-	axios.get(drawURL)
-		.then((res) => {
-			if (res.data.remaining == 0) {
-				draw.style.display = "none";
-				shuffle.style.display = "block";
-				table.innerHTML = "";
-				cardCount = 0;
-				return;
-			}
-			card = res.data.cards[0];
-			const suit = card.suit;
-			const value = card.value;
-			const title = `${value} of ${suit}`;
-			const cardImage = document.createElement("img");
-			cardImage.src = card.image;
-			cardImage.alt = card.title;
-			cardImage.style.position = "absolute";
-			cardImage.style.zIndex = String(cardCount);
-			table.appendChild(cardImage);
-			console.log(res.data.cards[0]);
-			console.log(title);
-			cardCount++;
-		})
-		.catch((err) => console.log(err));
+	card = cards.next().value;
+
+	if (!card) {
+		shuffle.style.display = "block";
+		draw.style.display = "none";
+		table.innerHTML = "";
+		return;
+	}
+
+	const suit = card.suit;
+	const value = card.value;
+	const title = `${value} OF ${suit}`;
+
+	const cardElement = document.createElement("img");
+	cardElement.src = card.image;
+	cardElement.alt = title;
+	cardElement.style.position = "absolute";
+	cardElement.style.transform = rotate();
+
+	table.appendChild(cardElement);
 });
+//
